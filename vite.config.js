38355baf2932b718165e5fbb06049b444c83a5d9
Max1ev <kapas.max@gmail.com>
2025-09-30 20:40:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
-  base: '/goit-js-hw-11/',
+  base: '/goit-js-hw-12/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
